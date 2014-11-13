6555756fd070a159b16754ed8f0693357af863e2
Nicolas CAN <nicolas.can@univ-lille1.fr>
2014-11-13 10:02:45 +0000

    move scritp tag in dmcloud.html to load it only if html5 player is used. convert dmcloud.py to pep8 compliant

--- dmcloud/dmcloud.py
+++ dmcloud/dmcloud.py
@@ -13,7 +13,7 @@ import os
 
 # Set up message catalog access
 from django.utils import translation as trans
-LANGUAGE=trans.get_language()
+LANGUAGE = trans.get_language()
 t = gettext.translation('dmcloud', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'locale'), languages=[LANGUAGE], fallback=True)
 _ = t.ugettext
 
@@ -23,9 +23,10 @@ from xblock.fields import Scope, Integer, String, Boolean
 from xblock.fragment import Fragment
 from django.template import Context, Template
 
-from webob import Response #send response from handler
+from webob import Response  # send response from handler
 
-import time, sys
+import time
+import sys
 
 ################################################################################
 #  Globals Var #################################################################
@@ -42,6 +43,7 @@ try:
     from universities.models import University
 except:
     log.error("You have to install universities application before using this block")
+
 
 ################################################################################
 class DmCloud(XBlock):
@@ -66,13 +68,13 @@ class DmCloud(XBlock):
     # Fields are defined on the class.  You can access them in your code as
     # self.<fieldname>.
     #title = String(help=_('Title of the video'), default=_('My new video'), scope=Scope.content, display_name=_('Title'))
-    
+
     video_time = 0
     icon_class = 'video'
 
     # To make sure that js files are called in proper order we use numerical
     # index. We do that to avoid issues that occurs in tests.
-    
+
     #display name already defined by xblock - we just redefined it to update translation
     display_name = String(
         help=_("The name students see. This name appears in the course ribbon and as a header for the video."),
@@ -80,40 +82,40 @@ class DmCloud(XBlock):
         default=_("New video"),
         scope=Scope.settings
     )
-    
+
     id_video = String(
         scope=Scope.settings,
         help=_('Fill this with the ID of the video found on DM Cloud'),
         default="",
         display_name=_('Video ID')
     )
-    
+
     allow_download_video = Boolean(
         help=_("Allow students to download this video."),
         display_name=_("Video Download Allowed"),
         scope=Scope.settings,
         default=False
     )
-    
+
     saved_video_position = Integer(
         help="Current position in the video.",
         scope=Scope.user_state,
         default=0
     )
-    
+
     playback_rate = String(
         display_name=_("Playback rate"),
         help=_("Change current speed of the video"),
         scope=Scope.user_state,
         values=[
-            {'name': "2 (%s)"%_("Faster"), 'val': "2.0"},
-            {'name': "1.5 (%s)"%_("Faster"), 'val': "1.5"},
-            {'name': "%s"%_("Real time"), 'val': "1.0"},
-            {'name': "0.7 (%s)"%_("Slower"), 'val': "0.7"},
-            {'name': "0.5 (%s)"%_("Slower"), 'val': "0.5"},
+            {'name': "2 (%s)" % _("Faster"), 'val': "2.0"},
+            {'name': "1.5 (%s)" % _("Faster"), 'val': "1.5"},
+            {'name': "%s" % _("Real time"), 'val': "1.0"},
+            {'name': "0.7 (%s)" % _("Slower"), 'val': "0.7"},
+            {'name': "0.5 (%s)" % _("Slower"), 'val': "0.5"},
         ],
         default="1.0"
-        )
+    )
 
     player = String(
         display_name=_("Player"),
@@ -124,7 +126,7 @@ class DmCloud(XBlock):
             {'name': "Dailymotion", 'val': "Dailymotion"},
         ],
         default="HTML5"
-        )
+    )
 
     def resource_string(self, path):
         """Gets the content of a resource"""
@@ -159,7 +161,7 @@ class DmCloud(XBlock):
         if self.id_video != "":
             try:
                 if self.player == "Dailymotion":
-                    embed_url = self.cloudkey.media.get_embed_url(id=self.id_video, expires = time.time() + 3600 * 24 * 7)
+                    embed_url = self.cloudkey.media.get_embed_url(id=self.id_video, expires=time.time() + 3600 * 24 * 7)
                 else:
                     #assets['jpeg_thumbnail_source']['stream_url']
                     #mp4_h264_aac
@@ -168,26 +170,31 @@ class DmCloud(XBlock):
                     #mp4_h264_aac_hd -> 720
                     #jpeg_thumbnail_medium
                     thumbnail_url = self.cloudkey.media.get_stream_url(id=self.id_video, asset_name='jpeg_thumbnail_source')
-                    stream_url = self.cloudkey.media.get_stream_url(id=self.id_video, expires = time.time() + 3600 * 24 * 7)
+                    stream_url = self.cloudkey.media.get_stream_url(id=self.id_video, expires=time.time() + 3600 * 24 * 7)
                     assets = self.cloudkey.media.get_assets(id=self.id_video)
-                    if assets.get('mp4_h264_aac_hd') :
-                        stream_url_hd = self.cloudkey.media.get_stream_url(id=self.id_video, asset_name='mp4_h264_aac_hd', expires = time.time() + 3600 * 24 * 7)
-                    elif assets.get('mp4_h264_aac_hq') :
-                        stream_url_hd = self.cloudkey.media.get_stream_url(id=self.id_video, asset_name='mp4_h264_aac_hq', expires = time.time() + 3600 * 24 * 7)
+                    if assets.get('mp4_h264_aac_hd'):
+                        stream_url_hd = self.cloudkey.media.get_stream_url(
+                            id=self.id_video, asset_name='mp4_h264_aac_hd', expires=time.time() + 3600 * 24 * 7)
+                    elif assets.get('mp4_h264_aac_hq'):
+                        stream_url_hd = self.cloudkey.media.get_stream_url(
+                            id=self.id_video, asset_name='mp4_h264_aac_hq', expires=time.time() + 3600 * 24 * 7)
                     #assets = self.cloudkey.media.get_assets(id=self.id_video)
-                    subs_url = self.cloudkey.media.get_subs_urls(id=self.id_video, type="srt")
-                    if self.allow_download_video :
-                        download_url_ld = self.cloudkey.media.get_stream_url(id=self.id_video, asset_name='mp4_h264_aac_ld', download=True, expires = time.time() + 3600 * 24 * 7)
-                        download_url_std = self.cloudkey.media.get_stream_url(id=self.id_video, download=True, expires = time.time() + 3600 * 24 * 7)
-                        download_url_hd = self.cloudkey.media.get_stream_url(id=self.id_video, asset_name='mp4_h264_aac_hd', download=True, expires = time.time() + 3600 * 24 * 7)
+                    subs_url = self.cloudkey.media.get_subs_urls(
+                        id=self.id_video, type="srt")
+                    if self.allow_download_video:
+                        download_url_ld = self.cloudkey.media.get_stream_url(
+                            id=self.id_video, asset_name='mp4_h264_aac_ld', download=True, expires=time.time() + 3600 * 24 * 7)
+                        download_url_std = self.cloudkey.media.get_stream_url(
+                            id=self.id_video, download=True, expires=time.time() + 3600 * 24 * 7)
+                        download_url_hd = self.cloudkey.media.get_stream_url(
+                            id=self.id_video, asset_name='mp4_h264_aac_hd', download=True, expires=time.time() + 3600 * 24 * 7)
             except:
-                import sys
-                msg = u'\n***** DmCloud error :%s - %s' %(sys.exc_info()[0], sys.exc_info()[1])
+                msg = u'\n***** DmCloud error :%s - %s' % (sys.exc_info()[0], sys.exc_info()[1])
                 log.error(msg)
                 print msg
-        
-        #change #892 
-        videojsurl = self.runtime.local_resource_url(self,"public/video-js-4.6-full/video.js")
+
+        #create url for videojs to add it directly in the template
+        videojsurl = self.runtime.local_resource_url(self, "public/video-js-4.6-full/video.js")
 
         frag.add_content(self.render_template("templates/html/dmcloud.html", {
             'self': self,
@@ -196,30 +203,28 @@ class DmCloud(XBlock):
             'download_url_ld': download_url_ld,
             'download_url_std': download_url_std,
             'download_url_hd': download_url_hd,
-            'stream_url' : stream_url,
-            'stream_url_hd' : stream_url_hd,
-            'subs_url' : subs_url,
-            'thumbnail_url' :thumbnail_url,
-            "transcript_url" : self.runtime.handler_url(self, 'transcript', 'translation').rstrip('/?'),
-            "videojsurl" : videojsurl
+            'stream_url': stream_url,
+            'stream_url_hd': stream_url_hd,
+            'subs_url': subs_url,
+            'thumbnail_url': thumbnail_url,
+            "transcript_url": self.runtime.handler_url(self, 'transcript', 'translation').rstrip('/?'),
+            "videojsurl": videojsurl
         }))
-        
-        
+
         #frag.add_css_url("public/videojs-4.6/video-js.css")
-        frag.add_css_url(self.runtime.local_resource_url(self,"public/video-js-4.6-full/video-js.min.css"))
-        #frag.add_css_url("http://vjs.zencdn.net/4.6/video-js.css")
+        frag.add_css_url(self.runtime.local_resource_url(self, "public/video-js-4.6-full/video-js.min.css"))
         #load locally to work with more than one instance on page
-        
+
         #frag.add_javascript(self.resource_string("public/video-js-4.6-full/video.js"))
         #frag.add_javascript_url(self.runtime.local_resource_url(self,"public/video-js-4.6-full/video.js"))
         frag.add_javascript(self.resource_string("public/js/src/dmcloud-video.js"))
-        
+
         #frag.add_css(self.resource_string("public/css/dmcloud.css"))
-        frag.add_css_url(self.runtime.local_resource_url(self,"public/css/dmcloud.css"))
+        frag.add_css_url(self.runtime.local_resource_url(self, "public/css/dmcloud.css"))
         frag.initialize_js('DmCloudVideo')
-        
+
         return frag
-    
+
     def studio_view(self, context=None):
         """
         Editing view in Studio
@@ -229,11 +234,11 @@ class DmCloud(XBlock):
         frag.add_css(self.resource_string("public/css/dmcloud.css"))
         frag.add_javascript(self.resource_string("public/js/src/dmcloud.js"))
         frag.initialize_js('DmCloud')
-        return frag    
-    
+        return frag
+
     @XBlock.json_handler
     def studio_submit(self, submissions, suffix=''):
-        if submissions['id_video']== "":
+        if submissions['id_video'] == "":
             response = {
                 'result': 'error',
                 'message': 'You should give a video ID'
@@ -248,29 +253,29 @@ class DmCloud(XBlock):
                 'result': 'success',
             }
         return response
-    
+
     @XBlock.json_handler
     def save_user_state(self, submissions, suffix=''):
         #print u'Received submissions: {}'.format(submissions)
         #log.info(u'Received submissions: {}'.format(submissions))
         self.saved_video_position = submissions['saved_video_position']
         response = {
-                'result': 'success',
-            }
+            'result': 'success',
+        }
         return response
-        
+
     @XBlock.handler
     def transcript(self, request, dispatch):
         #print "REQUEST GET: %s" %request.GET.get('url')
         if request.GET.get('url'):
             import requests
             r = requests.get(request.GET.get('url'))
-            response = Response("%s" %r.content)
+            response = Response("%s" % r.content)
             response.content_type = 'text/plain'
         else:
             response = Response(status=404)
         return response
-        
+
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
