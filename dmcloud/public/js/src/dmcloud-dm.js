14f1488019c51deb341a7aba816f145a304958f9
Nicolas CAN <nicolas.can@univ-lille1.fr>
2014-11-21 10:15:40 +0000

    import dmjs sdk to load it locally - modifiy dmcloud-dm.js to load player dynamically with initialise player function - test if already initialise before

--- dmcloud/public/js/src/dmcloud-dm.js
+++ dmcloud/public/js/src/dmcloud-dm.js
@@ -1,4 +1,3 @@
-
 var is_saving_user_state = false;
 
 function DmCloudPlayer(runtime, element) {
@@ -10,28 +9,34 @@ function DmCloudPlayer(runtime, element) {
 window.dmAsyncInit = function()
 {
     //console.log("dmAsyncInit");
-    
+    initialisePlayer();
+};
+
+function initialisePlayer(){
     $(".dmplayer").each(function(){
-        console.log(this.id);
-        var video = $(this).attr('data-video');
-        var saveurl = $(this).attr('data-save');
-        var params = {
-            auth: $(this).attr('data-auth'),
-        };
-        var player = DM.player(this.id, {video: video, width: "100%", height: "360", params: params});
-        player.addEventListener("apiready", function(e)
-        {
-            //e.target.play();
-            //console.log("apiready : " + this.id);
-        });
-        if(saveurl) {
-            player.addEventListener('seeked', function(){ the_save_user_state(this, saveurl);});
-            player.addEventListener('ended', function(){ the_save_user_state(this, saveurl);});
-            player.addEventListener('pause', function(){ the_save_user_state(this, saveurl);});
+        //console.log(this.id+" - "+this.tagName+" - "+$(this).is('div'));
+        if($(this).is('div')){
+            var video = $(this).attr('data-video');
+            var saveurl = $(this).attr('data-save');
+            console.log(this.id+" - "+video+" - "+saveurl);
+            var params = {
+                auth: $(this).attr('data-auth'),
+            };
+            var player = DM.player(this.id, {video: video, width: "100%", height: "360", params: params});
+            player.addEventListener("apiready", function(e)
+            {
+                //e.target.play();
+                //console.log("apiready : " + this.id);
+            });
+            if(saveurl) {
+                player.addEventListener('seeked', function(){ the_save_user_state(this, saveurl);});
+                player.addEventListener('ended', function(){ the_save_user_state(this, saveurl);});
+                player.addEventListener('pause', function(){ the_save_user_state(this, saveurl);});
+            }
         }
     });
-    
-};
+}
+
 
 function the_save_user_state(theplayer, saveurl) {
     //console.log(theplayer+" - "+saveurl+" - "+parseInt(theplayer.currentTime));
