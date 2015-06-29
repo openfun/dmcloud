4b8f034f16eca36d8a6798adb8957de237f849e9
Richard Moch <richard.moch@gmail.com>
2015-06-29 15:29:42 +0000

    avoid js error if video element has no id attribute

--- dmcloud/public/js/src/dmcloud-video.js
+++ dmcloud/public/js/src/dmcloud-video.js
@@ -1,79 +1,81 @@
 function DmCloudVideo(runtime, element) {
     var saveHandlerUrl = runtime.handlerUrl(element, 'save_user_state');
-    
     var myPlayer;
-    var video_id = $(element).find('video').attr('id');
-    var global_id = video_id.replace(/video_/, '');
-    var hdurl = $(element).find('video').attr('HD');
-    var sdurl = $(element).find('video').attr('SD');
-    var select_id = $(element).find('select').attr('id');
-    var subtitle_id = $(element).find('.dm-subtitle').attr('id');
-    var videoplayer_id = $(element).find('.videoplayer').attr('id');
-    var newtop =0;
-    var videoHD=false;
-    /**
-    HD
-    **/
-    if(video_id) {
-        videojs.HDPlugin = videojs.Button.extend({
-            /* @constructor */
-            init: function(player, options){
-                this.player = player;
-                player.videoHD = options.videoHD;
-                videojs.Button.call(this, player, options);
-                this.on('click', this.onClick);
-            }
-        });
-
-        videojs.HDPlugin.prototype.onClick = function() {
-            
-            var player = this.player,
-                current_time = player.currentTime(),
-                is_paused = player.paused();
-
-            if(player.videoHD==true){
-
-                $("#"+player.id()).find(".vjs-HD-button").removeClass("hdon");
-                $("#"+player.id()).find(".vjs-HD-button").addClass("hdoff");
-
-                player.src( [{type: "video/mp4", src: player.sdurl }] ).one( 'loadedmetadata', function() {
-                    player.currentTime( current_time );
-                    if ( !is_paused ) { player.play(); }
-                });
-                player.videoHD = false;
-            } else {
-                $("#"+player.id()).find(".vjs-HD-button").removeClass("hdoff");
-                $("#"+player.id()).find(".vjs-HD-button").addClass("hdon");
-
-                player.src( [{type: "video/mp4", src: player.hdurl }] ).one( 'loadedmetadata', function() {
-                    player.currentTime( current_time );
-                    if ( !is_paused ) { player.play(); }
-                });
-                player.videoHD = true;
-            }
-        };
-
-        videojs.plugin('HDPlugin', function( options ) {
-            var player = this;
-            player.chgsrc = function(src) {
-                player.src([{type: "video/mp4", src: src }]);
-                player.play();
-            };
-            var HDPlugin = new videojs.HDPlugin( this, {
-                el : videojs.Component.prototype.createEl( null, {
-                    className: 'vjs-HD-button vjs-control',
-                       innerHTML: '<div class="vjs-control-content">' + ('HD') + '</div>',
-                       role: 'button',
-                       'aria-live': 'polite', 
-                       tabIndex: 0
-                    
-                }),
-                videoHD : false
+    var $videoElement = $(element).find('video');
+    var video_id = $videoElement.attr('id');
+    if (video_id) {
+        var global_id = video_id.replace(/video_/, '');
+        var hdurl = $(element).find('video').attr('HD');
+        var sdurl = $(element).find('video').attr('SD');
+        var select_id = $(element).find('select').attr('id');
+        var subtitle_id = $(element).find('.dm-subtitle').attr('id');
+        var videoplayer_id = $(element).find('.videoplayer').attr('id');
+        var newtop =0;
+        var videoHD=false;
+        /**
+        HD
+        **/
+        if(video_id) {
+            videojs.HDPlugin = videojs.Button.extend({
+                /* @constructor */
+                init: function(player, options){
+                    this.player = player;
+                    player.videoHD = options.videoHD;
+                    videojs.Button.call(this, player, options);
+                    this.on('click', this.onClick);
+                }
             });
-            // Add the button to the control bar object and the DOM
-            this.controlBar.HDPlugin = this.controlBar.addChild( HDPlugin );
-        });
-    }
+
+            videojs.HDPlugin.prototype.onClick = function() {
+
+                var player = this.player,
+                    current_time = player.currentTime(),
+                    is_paused = player.paused();
+
+                if(player.videoHD==true){
+
+                    $("#"+player.id()).find(".vjs-HD-button").removeClass("hdon");
+                    $("#"+player.id()).find(".vjs-HD-button").addClass("hdoff");
+
+                    player.src( [{type: "video/mp4", src: player.sdurl }] ).one( 'loadedmetadata', function() {
+                        player.currentTime( current_time );
+                        if ( !is_paused ) { player.play(); }
+                    });
+                    player.videoHD = false;
+                } else {
+                    $("#"+player.id()).find(".vjs-HD-button").removeClass("hdoff");
+                    $("#"+player.id()).find(".vjs-HD-button").addClass("hdon");
+
+                    player.src( [{type: "video/mp4", src: player.hdurl }] ).one( 'loadedmetadata', function() {
+                        player.currentTime( current_time );
+                        if ( !is_paused ) { player.play(); }
+                    });
+                    player.videoHD = true;
+                }
+            };
+
+            videojs.plugin('HDPlugin', function( options ) {
+                var player = this;
+                player.chgsrc = function(src) {
+                    player.src([{type: "video/mp4", src: src }]);
+                    player.play();
+                };
+                var HDPlugin = new videojs.HDPlugin( this, {
+                    el : videojs.Component.prototype.createEl( null, {
+                        className: 'vjs-HD-button vjs-control',
+                           innerHTML: '<div class="vjs-control-content">' + ('HD') + '</div>',
+                           role: 'button',
+                           'aria-live': 'polite',
+                           tabIndex: 0
+
+                    }),
+                    videoHD : false
+                });
+                // Add the button to the control bar object and the DOM
+                this.controlBar.HDPlugin = this.controlBar.addChild( HDPlugin );
+            });
+        }
+    } // id $videoElement
     /**
     fin HD
     **/
@@ -83,7 +85,7 @@ function DmCloudVideo(runtime, element) {
             $("#"+subtitle_id).show();
             //$("#"+videoplayer_id).attr('style','width:55%;float:left');
             $("#"+videoplayer_id).attr('style','width:61%;float:left');
-            $("#"+videoplayer_id).find('.vjs-control[role="button"]').css('width','3em');            
+            $("#"+videoplayer_id).find('.vjs-control[role="button"]').css('width','3em');
         }
         //console.log("showCues "+subtitle_id);
         document.getElementById(subtitle_id).innerHTML = ""; //Open/Close <span class=\"togglesub\">&nbsp;</span><br/>
@@ -112,9 +114,9 @@ function DmCloudVideo(runtime, element) {
                }, 1000);
            });
         }
-        
+
     }
-    
+
     var trackload = new Array(); // array contening tracks id and cues (queues ?)
 
     /* Here's where you'd do things on page load. */
@@ -134,9 +136,9 @@ function DmCloudVideo(runtime, element) {
                     save_user_state(saveHandlerUrl);
                     log('stop_video', { 'currentTime': parseInt(myPlayer.currentTime())} );
                 });
-                myPlayer.on('pause', function(){ 
+                myPlayer.on('pause', function(){
                     save_user_state(saveHandlerUrl);
-                    log('pause_video', { 'currentTime': parseInt(myPlayer.currentTime()) }); 
+                    log('pause_video', { 'currentTime': parseInt(myPlayer.currentTime()) });
                 });
                 myPlayer.on('play', function(){ log( 'play_video', { 'currentTime': parseInt(myPlayer.currentTime()) }) });
                 myPlayer.on('loadstart', function(){ log('load_video', {}) });
@@ -194,42 +196,42 @@ function DmCloudVideo(runtime, element) {
         if($("#"+subtitle_id).is(':visible')) {
             $("#"+subtitle_id).hide();
             $("#"+videoplayer_id).attr('style','width:100%;float:left');
-            $("#"+videoplayer_id).find('.vjs-control[role="button"]').css('width','4em');  
+            $("#"+videoplayer_id).find('.vjs-control[role="button"]').css('width','4em');
             log('video_hide_subtitle', { 'currentTime': parseInt(myPlayer.currentTime()), } );
         }else{
             $("#"+subtitle_id).show();
             $("#"+videoplayer_id).attr('style','width:61%;float:left');
-            $("#"+videoplayer_id).find('.vjs-control[role="button"]').css('width','3em');  
+            $("#"+videoplayer_id).find('.vjs-control[role="button"]').css('width','3em');
             log('video_show_subtitle', { 'currentTime': parseInt(myPlayer.currentTime()), } );
         }
     });
     /**
-    event to track into tracking file play back rate 
+    event to track into tracking file play back rate
     **/
     $(element).find('.vjs-playback-rate').click(function() {
-        log('speed_change_video', { 
+        log('speed_change_video', {
                 'currentTime': parseInt(myPlayer.currentTime()),
                 'newSpeed' : myPlayer.playbackRate()
         });
     });
     /**
-    send log event to save it into tracking file 
+    send log event to save it into tracking file
     **/
     function log(eventName, data) {
         var logInfo;
-    
+
         // Default parameters that always get logged.
         logInfo = {
             id: global_id
         };
-    
+
         // If extra parameters were passed to the log.
         if (data) {
             $.each(data, function (paramName, value) {
                 logInfo[paramName] = value;
             });
         }
-    
+
         Logger.log(eventName, logInfo);
     }
 }
